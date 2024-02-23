bea3e7fb5c199fcb041e5851c54191d4080159b3
Faisal <faisalmaya@hotmail.com>
2024-02-23 15:23:23 +0000

    nav test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
     <main>
+      <Navbar/>
       <h1 className='text-4xl'>hello world</h1>
     </main>
   )
